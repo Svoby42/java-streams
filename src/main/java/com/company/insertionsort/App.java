9160eaeabaa9267cd4b94b7c8a5ef1d0439a0c43
Svoby42 <35143056+Svoby42@users.noreply.github.com>
2022-02-14 02:21:27 +0100

    sort repete

---
+++ src/main/java/com/company/insertionsort/App.java
@@ -0,0 +1,25 @@
+package com.company.insertionsort;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        int[] pole = {45,5,6,9987,32,145,7,8,654};
+        System.out.println(Arrays.toString(insertionSort(pole)));
+    }
+
+    public static int[] insertionSort(int[] a){
+        for(int i = 1; i <= a.length-1; i++){
+            int x = a[i];
+            int j = i - 1;
+            while(j >= 0 && a[j] > x){
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1]=x;
+        }
+        return a;
+    }
+
+}
